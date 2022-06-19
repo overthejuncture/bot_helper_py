74c30c561c931cb933a97d6b48c261ee26d79dc9
overthejuncture <overthejuncture@gmail.com>
2022-06-19 20:32:58 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='Package check',
+    url='https://github.com/overthejuncture/bot_helper_py.git',
+    author='OTJ',
+    author_email='overthejuncure@gmail.com',
+    # Needed to actually package something
+    packages=['bot'],
+    # Needed for dependencies
+    install_requires=[],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='An example of a python package from pre-existing code',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
