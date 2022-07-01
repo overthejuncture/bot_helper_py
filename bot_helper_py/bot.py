b96132ac0f1960026b269b1d053320c189c9cc23
overthejuncture <overthejuncture@gmail.com>
2022-07-01 03:24:47 +0300

    more actions

--- bot_helper_py/bot.py
+++ bot_helper_py/bot.py
@@ -1,8 +1,10 @@
+from tkinter import ACTIVE
 from typing import (
     List,
     Callable,
     Union,
-    Tuple
+    Tuple,
+    Dict
 )
 import logging
 import json
@@ -13,42 +15,54 @@ from telegram import (
     Update
 )
 
-from django.db.models import (
-    Model,
-    QuerySet
-)
+from django.db.models import Model
 
+ACTION_LITERAL = 'a'
+DATA_LITERAL = 'd'
+EDIT_ACTION = 'e'
+DELETE_ACTION = 'd'
+LIMIT_DEFAULT = 3 
 
 def check(update: Update):
     print(update.message.from_user.id)
 
-def listAll(list: List[Model], field: Callable = lambda x:x.name) -> str:
+def list_all(list: List[Model], field: Callable = lambda x:x.name) -> str:
     return "\n".join("{idx}. {text}".format(idx=idx+1, text=field(x)) for idx, x in enumerate(list))
 
-def list_with_keyboard(
+def list_with_keyboard_and_pager(
     list: List[Model],
     start: int = 0,
-    limit: int = 10,
+    limit: int = LIMIT_DEFAULT,
     field: Callable = lambda x:x.name,
-    n_cols: int = 2
+    n_cols: int = 2,
+):
+    return _list_with_keyboard(list, start, limit, field, n_cols, make_pager=True)
+
+def _list_with_keyboard(
+    list: List[Model],
+    start: int = 0,
+    limit: int = LIMIT_DEFAULT,
+    field: Callable = lambda x:x.name,
+    n_cols: int = 2,
+    make_pager: bool = False
 ):
     list = list.all()[start:start+limit+1]
 
     show_next = False if len(list) != limit+1 else True
     show_prev = False if start == 0 else True
 
-    list = list[0:10]
-    text = listAll(list, field)
+    list = list[0:limit]
+    text = list_all(list, field)
     buttons = []
     for idx, item in enumerate(list):
-        buttons.append(InlineKeyboardButton(text=idx + 1, callback_data=item.pk))
-    footer_buttons = make_footer_buttons(start, limit, show_prev=show_prev, show_next=show_next)
+        buttons.append(InlineKeyboardButton(text=idx + 1, callback_data=json.dumps({DATA_LITERAL: {'id': item.pk}})))
+    footer_buttons = make_pager_buttons(start, limit, show_prev=show_prev, show_next=show_next) if make_pager else []
     reply_markup = InlineKeyboardMarkup(build_menu(buttons, n_cols, footer_buttons=footer_buttons))
     return text, reply_markup
 
-def make_footer_buttons(
+def make_pager_buttons(
     start: int = 0,
-    limit: int = 10,
+    limit: int = LIMIT_DEFAULT,
     show_prev: bool = True,
     show_next: bool = True
 ) -> List[InlineKeyboardButton]:
@@ -68,7 +82,7 @@ def make_footer_buttons(
     return buttons
 
 
-def make_previous_button(start: int = 0, limit: int = 10):
+def make_previous_button(start: int = 0, limit: int = LIMIT_DEFAULT):
     start = start - limit if start - limit > 0 else 0
     button = InlineKeyboardButton('<', callback_data=json.dumps({'start': start, 'limit': limit}))
 
@@ -91,3 +105,11 @@ def proccess_pager(update: Update) -> Tuple[int, int]:
     start = data.get('start')
     limit = data.get('limit')
     return int(start), int(limit)
+
+def create_action_buttons(data: Dict = {}, edit: bool = False, delete: bool = False) -> List[List[InlineKeyboardButton]]:
+    buttons = []
+    if edit:
+        buttons.append(InlineKeyboardButton('Изменить', callback_data=json.dumps({ACTION_LITERAL: EDIT_ACTION, DATA_LITERAL: data})))
+    if delete:
+        buttons.append(InlineKeyboardButton('Удалить', callback_data=json.dumps({ACTION_LITERAL: DELETE_ACTION, DATA_LITERAL: data})))
+    return [buttons]
--- bot_helper_py/callback_query.py
+++ bot_helper_py/callback_query.py
@@ -1,6 +1,17 @@
+from ast import Call
 from telegram.callbackquery import CallbackQuery
 import json
+from . import bot
 
 def is_pager_action(callback: CallbackQuery):
     data = json.loads(callback.data)
     return True if data.get('start') != None else False
+
+def get_action(callback: CallbackQuery):
+    data = json.loads(callback.data)
+    action = data.get(bot.ACTION_LITERAL)
+    return action, data.get(bot.DATA_LITERAL)
+
+def get_data(callback: CallbackQuery):
+    data = json.loads(callback.data)
+    return data.get(bot.DATA_LITERAL)
